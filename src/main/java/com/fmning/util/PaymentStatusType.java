2198ed0a266236afe63287290469ed5a9aae5bd4
Fangming Ning <synfm@126.com>
2018-01-16 16:15:38 -0500

    Adding payment status

--- src/main/java/com/fmning/util/PaymentStatusType.java
+++ src/main/java/com/fmning/util/PaymentStatusType.java
@@ -1,7 +1,11 @@
 package com.fmning.util;
 
+//TODO: Status should only be Done, Not exists or Rejected
 public enum PaymentStatusType {
-	DONE("Done");
+	DONE("Done"),
+	ALREADY_PAID("AlreadyPaid"),//TODO: Remove this
+	NOT_EXIST("NotExist"),
+	REJECTED("Rejected");
 	
 	private final String name;
 	
